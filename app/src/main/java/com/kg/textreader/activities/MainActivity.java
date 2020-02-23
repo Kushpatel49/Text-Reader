710499a988110cfccc5e34db937de7224424c514
Kush Patel <kushpatel49@outlook.com>
2020-02-23 21:51:45 +0530

    Text Scanner using Google Vision API

---
+++ app/src/main/java/com/kg/textreader/activities/MainActivity.java
@@ -0,0 +1,112 @@
+package com.kg.textreader.activities;
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.teapink.ocr_reader.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CompoundButton useFlash;
+    private TextView statusMessage;
+    private TextView textValue;
+    //private Button copyButton;
+    //private Button mailTextButton;
+
+    private static final int RC_OCR_CAPTURE = 9003;
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        statusMessage = (TextView)findViewById(R.id.status_message);
+        textValue = (TextView)findViewById(R.id.text_value);
+        useFlash = (CompoundButton) findViewById(R.id.use_flash);
+
+        Button readTextButton = (Button) findViewById(R.id.read_text_button);
+        readTextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // launch Ocr capture activity.
+                Intent intent = new Intent(getApplicationContext(), OcrCaptureActivity.class);
+                intent.putExtra(OcrCaptureActivity.AutoFocus, true);
+                intent.putExtra(OcrCaptureActivity.UseFlash, useFlash.isChecked());
+
+                startActivityForResult(intent, RC_OCR_CAPTURE);
+            }
+        });
+
+
+
+        //mailTextButton = (Button) findViewById(R.id.mail_text_button);
+        //mailTextButton.setOnClickListener(new View.OnClickListener() {
+        //@Override
+        //public void onClick(View v) {
+        //  Intent i = new Intent(Intent.ACTION_SEND);
+        // i.setType("message/rfc822");
+        //i.putExtra(Intent.EXTRA_SUBJECT, "Text Read");
+        //i.putExtra(Intent.EXTRA_TEXT, textValue.getText().toString());
+        //try {
+        //    startActivity(Intent.createChooser(i, getString(R.string.mail_intent_chooser_text)));
+        //} catch (android.content.ActivityNotFoundException ex) {
+        //   Toast.makeText(getApplicationContext(),
+        //         R.string.no_email_client_error, Toast.LENGTH_SHORT).show();
+        //}
+        //}
+        // });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (textValue.getText().toString().isEmpty()) {
+            //copyButton.setVisibility(View.GONE);
+            //mailTextButton.setVisibility(View.GONE);
+        } else {
+            //copyButton.setVisibility(View.VISIBLE);
+            //mailTextButton.setVisibility(View.VISIBLE);
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == RC_OCR_CAPTURE) {
+            if (resultCode == CommonStatusCodes.SUCCESS) {
+                if (data != null) {
+                    String text = data.getStringExtra(OcrCaptureActivity.TextBlockObject);
+                    statusMessage.setText(R.string.ocr_success);
+                    textValue.setText(text);
+                    Log.d(TAG, "Text read: " + text);
+                } else {
+                    statusMessage.setText(R.string.ocr_failure);
+                    Log.d(TAG, "No Text captured, intent data is null");
+                }
+            } else {
+                statusMessage.setText(String.format(getString(R.string.ocr_error),
+                        CommonStatusCodes.getStatusCodeString(resultCode)));
+            }
+        }
+        else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+
+}
